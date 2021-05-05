0a6f6f198ec235969372e3362826a59dacb40df2
sysoyoung <sysoyoung@gmail.com>
2021-05-06 01:59:46 +0300

    try another branch

---
+++ config/tempDpListOfPages.js
@@ -0,0 +1,6 @@
+module.exports.tempArrayOfArticles = [
+    {id: 1, title: 'title one', author: 'max sys', date: +new Date() },
+    { id: 2, title: 'title two', author: 'max', date: +new Date() },
+    { id: 3, title: 'KEKW title', author: 'sys', date: +new Date() },
+    { id: 4, title: 'title four', author: 'kek 4ebuk', date: +new Date() },
+];
---
+++ routes/account.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/db');
+
+const user = require('../models/user');
+const User = user.User;
+
+router.post('/reg', (req, res) => {
+    
+    const newUser = new User(
+        req.body.name,
+        req.body.email,
+        req.body.password
+    );
+    const unswer = User.addUser(newUser);
+
+    res.send({ success: (unswer == 'User added'), message: unswer});
+});
+
+router.post('/auth', (req, res) => {
+    const password = req.body.password;
+    const email = req.body.email;
+
+    const user = User.getUserByEmail(email);
+    if(!user){
+        return res.json({success: false, message: 'email not found'});
+    }
+
+    User.comparePass(password, user.password, (err, isMatch) => {
+        if(err) throw err;
+
+        if(isMatch) {
+            const token = jwt.sign(user, config.secret, { expiresIn: "24h" });
+
+            res.json({
+                success: true,
+                token: 'JWT ' + token,
+                user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                }
+            })
+        } else {
+            return res.json({success: false, message: "wrong password"});
+        }
+    });
+});
+
+router.get('/dashboard', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.send('user cab');
+});
+
+// TESTINg ROUTS--------------
+
+router.get('/getusers', (req, res) => {
+    res.send((User.getUsers()));
+});
+
+router.get('/setuser', (req, res) => {
+    
+    let newUser = new User( 'qwe qwe', 'qwe@qwe.qwe', 'qweqwe');
+    let unswer = User.addUser(newUser);
+
+    res.send(unswer);
+});
+
+module.exports = router;
