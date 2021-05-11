c17af2b8915a719b1f53aacefc1e136e0cf850bb
sysoyoung <sysoyoung@gmail.com>
2021-05-12 02:47:41 +0300

    article creature

--- src/app/article-creation/article-creation.component.ts
+++ src/app/article-creation/article-creation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-article-creation',
@@ -8,26 +8,69 @@ import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class ArticleCreationComponent implements OnInit {
 
-  additionListControl!: FormArray;
+  articleFormGroup!: FormGroup;
 
-
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.additionListControl = this.formBuilder.array([this.createAdditionFromGroup()]);
 
-  }
-
-  createAdditionFromGroup(): FormGroup{
-    return this.formBuilder.group({
-      title: this.formBuilder.control('title'),
-      link: this.formBuilder.control('link')
+    this.articleFormGroup = this.fb.group({
+      title: ['Приклад хорошого заголовку', Validators.required],
+      lang: ['укр'],
+      description: ['Дуже короткий опис статті'],
+      sources: this.fb.array([
+        this.fb.group({
+          title: ['Приклад: Онтологічне моделювання'],
+          link: ['Приклад: https://trinidata.ru/files/SemanticIntro.pdf']
+        })
+      ]),
+      tags: this.fb.array([ ['Приклад: онтологія'] ]),
+      // related: this.fb.array([ ['rel arr'] ]),
+      text: [`Онтолоогія — вчення про буття, розділ філософії, у якому з'ясовуються фундаментальні проблеми існування, розвитку сутнісного, найважливішого.`]
     });
   }
 
-  pushAdditionFormGroup(): void{
-    this.additionListControl.push(this.createAdditionFromGroup());
+  getSourcesFormArray(): FormArray{
+    return this.articleFormGroup.get('sources') as FormArray;
   }
 
-  
+  getTagsFormArray(): FormArray{
+    return this.articleFormGroup.get('tags') as FormArray;
+  }
+
+  createNewSourceFromGroup(): void{
+    const group = this.fb.group({
+      title: [''],
+      link: ['']
+    });
+
+    this.getSourcesFormArray().push(group);
+  }
+
+  removeLastSourceFromGroup(event: any): void{
+    if (event.screenX === 0){
+      return;
+    }
+    this.getSourcesFormArray().removeAt( this.getSourcesFormArray.length - 1 );
+  }
+
+  createNewTagControl(): void{
+    this.getTagsFormArray().push(this.fb.control(''));
+  }
+
+  removeLastTagControl(): void{
+    this.getTagsFormArray().removeAt( this.getTagsFormArray().length - 1);
+  }
+
+  getSourceFormGroup(formGroupName: AbstractControl): FormGroup{
+    return formGroupName as FormGroup;
+  }
+
+  submit(): void{
+    if (this.articleFormGroup.valid){
+      console.log(this.articleFormGroup.value);
+    }
+    console.log('submited');
+  }
+
 }
